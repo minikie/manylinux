b0db596dc5098b0721c6be435ff4409df42e50fb
mathansang <minikie@kakao.com>
2023-01-18 10:30:39 +0900

    working...

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,21 @@
+
+build_id=$1
+
+function check_var {
+    if [ -z "$1" ]; then
+        echo "required variable not defined"
+        exit 1
+    fi
+}
+
 #!/bin/bash
 tag="montrixdev/${POLICY}_${PLATFORM}"
-build_id="2023-01-17"
+check_var ${build_id}
 # build_id=$(git show -s --format=%cd-%h --date=short ${COMMIT_SHA})
 
-docker login -u $QUAY_USERNAME -p $QUAY_PASSWORD quay.io
+# docker login -u $QUAY_USERNAME -p $QUAY_PASSWORD quay.io
+docker login -u montrixdev
+
 docker tag ${tag}:${COMMIT_SHA} ${tag}:${build_id}
 docker tag ${tag}:${COMMIT_SHA} ${tag}:latest
 docker push ${tag}:${build_id}
