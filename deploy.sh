30526f2ba24b6996b2e185841b86afdb70140131
mathansang <minikie@kakao.com>
2023-01-18 21:16:35 +0900

    working...

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,9 @@ tag="montrixdev/${POLICY}_${PLATFORM}"
 build_id=$(git show -s --format=%cd-%h --date=short ${COMMIT_SHA})
 
 echo "dockerhub login ( montrixdev )..."
+echo $DOCKERHUB_USERNAME 
+echo $DOCKERHUB_PASSWORD
+
 # docker login -u $QUAY_USERNAME -p $QUAY_PASSWORD quay.io
 docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD
 # docker login -u montrixdev -p ${password}
